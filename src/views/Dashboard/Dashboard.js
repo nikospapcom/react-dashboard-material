5d58e4913df04da44b224d4fa4d76d9674646c74
Nikos Pap <admin@nikospap.com>
2019-08-03 19:14:48 +0300

    intergate react helmet

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/styles';
-import { Grid } from '@material-ui/core';
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+import { Grid } from "@material-ui/core";
+import { Helmet } from "react-helmet";
 
 import {
   Budget,
@@ -11,7 +12,7 @@ import {
   UsersByDevice,
   LatestProducts,
   LatestOrders
-} from './components';
+} from "./components";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,80 +25,32 @@ const Dashboard = () => {
 
   return (
     <div className={classes.root}>
-      <Grid
-        container
-        spacing={4}
-      >
-        <Grid
-          item
-          lg={3}
-          sm={6}
-          xl={3}
-          xs={12}
-        >
+      <Helmet>
+        <title>Dashboard</title>
+      </Helmet>
+      <Grid container spacing={4}>
+        <Grid item lg={3} sm={6} xl={3} xs={12}>
           <Budget />
         </Grid>
-        <Grid
-          item
-          lg={3}
-          sm={6}
-          xl={3}
-          xs={12}
-        >
+        <Grid item lg={3} sm={6} xl={3} xs={12}>
           <TotalUsers />
         </Grid>
-        <Grid
-          item
-          lg={3}
-          sm={6}
-          xl={3}
-          xs={12}
-        >
+        <Grid item lg={3} sm={6} xl={3} xs={12}>
           <TasksProgress />
         </Grid>
-        <Grid
-          item
-          lg={3}
-          sm={6}
-          xl={3}
-          xs={12}
-        >
+        <Grid item lg={3} sm={6} xl={3} xs={12}>
           <TotalProfit />
         </Grid>
-        <Grid
-          item
-          lg={8}
-          md={12}
-          xl={9}
-          xs={12}
-        >
+        <Grid item lg={8} md={12} xl={9} xs={12}>
           <LatestSales />
         </Grid>
-        <Grid
-          item
-          lg={4}
-          md={6}
-          xl={3}
-          xs={12}
-        >
+        <Grid item lg={4} md={6} xl={3} xs={12}>
           <UsersByDevice />
         </Grid>
-        <Grid
-          item
-          lg={4}
-          md={6}
-          xl={3}
-          xs={12}
-        >
+        <Grid item lg={4} md={6} xl={3} xs={12}>
           <LatestProducts />
         </Grid>
-        <Grid
-          item
-          lg={8}
-          md={12}
-          xl={9}
-          xs={12}
-        >
+        <Grid item lg={8} md={12} xl={9} xs={12}>
           <LatestOrders />
         </Grid>
       </Grid>
